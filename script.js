3480ae9d93cea7018d0e6f936c976847ef401b9e
Julius Alamarvdashti <drako886@gmail.com>
2024-07-25 02:06:19 +0000

    Bugg fix infinite scroll

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-let copy = document.querySelector('.carousel-track').cloneNode(true);
-document.querySelector('.skills-carousel').appendChild(copy);
+// Select all elements with the class 'scrolling-text-item'
+let items = document.querySelectorAll('.scrolling-text-item');
+
+// Loop through each element and clone it
+items.forEach(item => {
+    let copy = item.cloneNode(true); // Clone the element
+    document.querySelector('.scrolling-text').appendChild(copy); // Append the cloned element
+});
